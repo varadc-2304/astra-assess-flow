241e436d487192fffb094b23879c258e73b4333a
Varad Charholikar <103520139+varadc-2304@users.noreply.github.com>
2025-04-18 00:54:15 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -79,9 +79,6 @@ const Login = () => {
           <CardFooter>
             <div className="w-full space-y-4">
               <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t" />
-                </div>
               </div>
             </div>
           </CardFooter>
